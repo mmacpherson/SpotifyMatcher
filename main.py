04505b6cc35a2bed7c5c350dcb44abf8686f9866
Mike Macpherson <mmacpherson@users.noreply.github.com>
2022-05-01 09:03:50 -0700

    drop comments / old functions

--- main.py
+++ main.py
@@ -11,26 +11,6 @@ import pandas as pd
 import spotipy
 from loguru import logger
 
-# import sys
-# from datetime import datetime
-# from time import sleep
-
-
-# from tinytag import TinyTag
-
-# def get_user_data():
-#     """Retrieve username and playlist id from arguments"""
-#     if len(sys.argv) == 2:
-#         return sys.argv[1], ""
-#     elif len(sys.argv) == 3:
-#         return sys.argv[1], sys.argv[2]
-#     else:
-#         print(
-#             f"Usage:\n\tpython {sys.argv[0]} username [OPTIONAL]playlist_id"
-#             "\n\nTo know how to find each, check the README.md or the GitHub page"
-#         )
-#         sys.exit()
-
 
 def chunk(items, size):
     if len(items) <= size:
@@ -82,16 +62,7 @@ def connect_to_spotify(username, scope):
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-    return spotify, auth_manager
-
-
-# def get_auth_token(auth_manager):
-#     auth_token = auth_manager.get_cached_token()
-
-#     if auth_token:
-#         return auth_token
-
-#     return auth_manager.get_access_token(as_dict=True)
+    return spotify
 
 
 def album_info(track: dict):
@@ -406,57 +377,6 @@ def create_spotify_playlist(
     return playlist_id
 
 
-# def ensure_playlist_exists(playlist_id):
-#     try:
-#         if not playlist_id:
-#             raise Exception
-#         sp.user_playlist(username, playlist_id)["id"]
-#         return playlist_id
-#     except:
-#         print(
-#             f"\nNo playlist_id provided. Creating a new playlist..."
-#             if len(playlist_id) == 0
-#             else f"\nThe playlist_id provided did not match any of your "
-#             "existing playlists. Creating a new one..."
-#         )
-#         return create_new_playlist()
-
-
-# def create_new_playlist():
-#     try:
-#         date = datetime.now().strftime("%d %b %Y at %H:%M")  # 1 Jan 2020 at 13:30
-#         playlist_id = sp.user_playlist_create(
-#             username,
-#             "SpotifyMatcher",
-#             description="Playlist automatically created by SpotifyMatcher "
-#             f"from my local files on {date}. "
-#             "Try it at https://github.com/BoscoDomingo/SpotifyMatcher!",
-#         )["id"]
-#         print(f"Find it at: https://open.spotify.com/playlist/{playlist_id}")
-#         return playlist_id
-#     except:
-#         print(
-#             "\nWARNING: \n"
-#             "There was an error creating the playlist. Please, create one "
-#             "manually and paste its id in the terminal, after your username\n"
-#         )
-#         sys.exit()
-
-
-# def add_tracks_to_playlist(track_ids):
-#     """Add tracks in batches of 100, since that's the limit Spotify has in place"""
-#     spotify_limit = 100
-#     while len(track_ids) > 0:
-#         try:
-#             sp.user_playlist_add_tracks(
-#                 username, playlist_id, track_ids[:spotify_limit]
-#             )
-#         except ValueError:  # API rate limit reached
-#             sleep(0.2)
-#         else:
-#             del track_ids[:spotify_limit]
-
-
 @click.command()
 @click.argument("username")
 @click.argument("music_dir", type=click.Path(exists=True))
@@ -480,16 +400,10 @@ def main(
     tracks = load_track_metadata(music_dir)
     logger.info(f"Discovered [{len(tracks)}] music files.")
 
-    # albums, tracks = cluster_albums(tracks)
-    # logger.info(
-    #     f"Discovered [{len(albums)}] albums, "
-    #     f"leaving [{len(tracks)}] tracks not associated with albums."
-    # )
-
     if not use_spotify:
         return
 
-    spotify, auth_manager = connect_to_spotify(username, spotify_scope)
+    spotify = connect_to_spotify(username, spotify_scope)
 
     # Search spotify for each track in collection.
     tracks_df = pd.DataFrame(spotify_batch_match_tracks(spotify, tracks)).assign(
@@ -518,64 +432,6 @@ def main(
 
     # Take tracks that match at spotify, but are not associated with an album.
     create_spotify_playlist(spotify, username, tracks_df, playlist_id=playlist_id)
-
-    #     # Needed to get the cached authentication if missing
-    #     dummy_search = sp.search("whatever", limit=1)
-
-    #     token_info = get_auth_token(auth_manager)
-
-    # track_ids = []
-    # failed_song_names = []
-    # searched_songs = 0
-
-    # with open(failed_matches_filename, "w") as failed_matches_file:
-
-    #     for query, song in get_title_and_artist(music_dir):
-
-    #         searched_songs += 1
-    #         print(f"{searched_songs}: {song}")
-
-    #         if spotify:
-    #             if auth_manager.is_token_expired(token_info):
-    #                 token_info = auth_manager.refresh_access_token(
-    #                     token_info["refresh_token"]
-    #                 )
-
-    #             try:
-    #                 result = sp.search(query, limit=3)
-    #                 print(result)
-    #                 if len(result) > 1:
-    #                     print("multiple hits")
-    #                 # ["tracks"]["items"][0]["id"]
-    #                 1 / 0
-    #             except ValueError:
-    #                 print("\t*NO MATCH*")
-    #                 failed_matches_file.write(f"{song}\n")
-    #                 failed_song_names.append(song)
-    #             else:
-    #                 track_ids.append(result)
-
-    #     success_rate = f"{len(track_ids) / (searched_songs - 1) * 100:.2f}"
-    #     print(
-    #         f"\n***TOTAL SONGS SEARCHED: {searched_songs}"
-    #         f"  TOTAL MATCHES:{len(track_ids)} ({success_rate}%)***\n"
-    #     )
-
-    # number_of_matches = len(track_ids)
-
-    # if spotify:
-    #     playlist_id = ensure_playlist_exists(playlist_id)
-    #     add_tracks_to_playlist(track_ids)
-
-    # print(
-    #     f"\nSuccessfully added {number_of_matches} songs to the playlist.\n"
-    #     "Thank you for using SpotifyMatcher!"
-    # )
-    # print(
-    #     f"\n{searched_songs-number_of_matches} UNMATCHED SONGS (search "
-    #     "for these manually, as they either have wrong info or aren't "
-    #     f'available in Spotify)\nWritten to "{failed_matches_filename}":\n'
-    # )
 
 
 if __name__ == "__main__":
